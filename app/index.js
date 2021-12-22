7ae10c65c11a18be995b65bd779d9e85accf5823
Alexandre J. Corrêa <alexandre.j.correa@hotmail.com>
2021-12-21 23:55:02 -0300

    Parsing Request Paths

--- app/index.js
+++ app/index.js
@@ -1,19 +1,34 @@
 /*
 * Primary file for the API
-*
-*
+* Author: Alexandre J. Corrêa
+* Date: 21/12/2021
 */
 
 //Dependecies
-var http = require('http');
+var http = require('http'),
+	url = require('url');
 
 // The server should respond to all requests with a string
-var server = http.createServer(function(rew, res){
+var server = http.createServer(function(req, res){
+	
+// Get the URL and parse ir
+var parsedUrl = url.parse(req.url, true);
+
+// Get the path
+var path = parsedUrl.pathname,
+	trimmedpath = path.replace(/^\/+|\/+$/g, '');
+
+
+// Send the response
 	res.end('Hello\n');
-})
+
+// Log the request path
+console.log('Request received on path: ' + trimmedpath);
+
+});
 
 
 // Start the server, and have it listen on a port 3000
 server.listen(3000, function(){
 	console.log("The server is listening on port 3000")
-})
+});
