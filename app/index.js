818d39bafd0cd50888093de26a049af75ba6c1ba
Alexandre J. Corrêa <alexandre.j.correa@hotmail.com>
2021-12-23 09:49:03 -0300

    Parsing headers

--- app/index.js
+++ app/index.js
@@ -10,7 +10,6 @@ var http = require('http'),
 
 // The server should respond to all requests with a string
 var server = http.createServer(function(req, res){
-	
 // Get the URL and parse ir
 var parsedUrl = url.parse(req.url, true);
 
